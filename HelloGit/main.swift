d1fdcdc031f8ae94bdf781e8196a24d739d96386
David Steuber <dsteuber@gmail.com>
2015-03-06 20:39:47 -0500

    Project creation.

---
+++ HelloGit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloGit
+//
+//  Created by David Steuber on 3/6/15.
+//  Copyright (c) 2015 David Steuber. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
